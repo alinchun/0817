4efade403070d7ed1df0011469a3f6d10b1ef522
chunlin <a655198@yahoo.com.tw>
2023-08-17 15:51:49 +0800

    datatable 串天氣

---
+++ data-api.js
@@ -0,0 +1,4 @@
+
+key="CWB-278790C2-1E11-4FBF-84A4-71F18CA1AA77";
+let url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-278790C2-1E11-4FBF-84A4-71F18CA1AA77';
+
